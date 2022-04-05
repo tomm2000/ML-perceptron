d35c47ee0b70292b307f0557d4cc1366ec1ab1c1
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2022-04-05 17:45:59 +0200

    xor

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     
+      <canvas id="glcanvas" tabindex='1'></canvas>
+      {/* <script src="/examples/xor.wasm"></script>
+      <script>load("/target/wasm32-unknown-unknown/release/examples/xor.wasm");</script> */}
     </div>
   );
 }
