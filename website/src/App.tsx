bf3338b8678bfde1abc38ba4ba5eb75e372426f7
tomm2000 <38072918+tomm2000@users.noreply.github.com>
2022-04-06 10:27:43 +0200

    new website

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,11 +1,9 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <canvas id="glcanvas" tabindex='1'></canvas>
-      {/* <script src="/examples/xor.wasm"></script>
-      <script>load("/target/wasm32-unknown-unknown/release/examples/xor.wasm");</script> */}
     </div>
   );
 }
